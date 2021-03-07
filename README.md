2f87b977b02757fc75f896d1bf14e0d24689194e
Uladzislau.Hrytsau <Vladboy45@gmail.com>
2021-03-07 19:26:51 +0300

    Configuration service

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# cash-resource-planner
-A cash planner can help you achieve a pleasant life, measure your expenses, and advise on what to get rid of.
+# Cash resource planner
+### A cash planner can help you achieve a pleasant life, measure your expenses, and advise on what to get rid of.
+***
+#### Main abilities:
+* ##### Track turnover
+* ##### to do 
+***
+#### Technology stack and libraries:
+* ##### Spring Boot
+* ##### Spring MVC
+* ##### Spring Data REST
+* ##### Spring Data JPA
+* ##### Spring Security
+* ##### Java JWT
+* ##### Spring Test
+* ##### JUnit
+* ##### Mockito
+* ##### AssertJ
+* ##### Lombok
+* ##### Maven
---
+++ crp-configuration-service/src/main/java/com/crp/ConfigurationServiceApplication.java
@@ -0,0 +1,22 @@
+package com.crp;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigurationServiceApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .logStartupInfo(true)
+                .web(WebApplicationType.SERVLET)
+                .build(args)
+                .run(args);
+    }
+
+}
