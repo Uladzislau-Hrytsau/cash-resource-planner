3cf5be1c3af839c367d22038bf3a9487f03d6bdd
Uladzislau.Hrytsau <Vladboy45@gmail.com>
2021-03-07 22:40:58 +0300

    fixed configuration

--- crp-configuration-service/src/main/java/com/crp/ConfigurationServiceApplication.java
+++ crp-configuration-service/src/main/java/com/crp/ConfigurationServiceApplication.java
@@ -1,6 +1,7 @@
 package com.crp;
 
 import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,12 +12,12 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigurationServiceApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ConfigurationServiceApplication.class)
-                .bannerMode(Banner.Mode.OFF)
-                .logStartupInfo(true)
-                .web(WebApplicationType.SERVLET)
-                .build(args)
-                .run(args);
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+//        new SpringApplicationBuilder(ConfigurationServiceApplication.class)
+//                .bannerMode(Banner.Mode.OFF)
+//                .logStartupInfo(true)
+//                .web(WebApplicationType.SERVLET)
+//                .build(args)
+//                .run(args);
     }
-
 }
--- crp-user-service/src/main/java/com/crp/UserServiceApplication.java
+++ crp-user-service/src/main/java/com/crp/UserServiceApplication.java
@@ -1,6 +1,7 @@
 package com.crp;
 
 import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,12 +12,13 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 public class UserServiceApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(UserServiceApplication.class)
-                .bannerMode(Banner.Mode.OFF)
-                .logStartupInfo(true)
-                .web(WebApplicationType.REACTIVE)
-                .build(args)
-                .run(args);
+        SpringApplication.run(UserServiceApplication.class, args);
+//        new SpringApplicationBuilder(UserServiceApplication.class)
+//                .bannerMode(Banner.Mode.OFF)
+//                .logStartupInfo(true)
+//                .web(WebApplicationType.REACTIVE)
+//                .build(args)
+//                .run(args);
     }
 
 }
